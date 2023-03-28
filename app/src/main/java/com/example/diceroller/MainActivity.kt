8d3a71ad38317059eb71a24ba4c2a323985cab13
Aiman Anizan <anizanaimann1@gmail.com>
2023-03-28 10:16:02 +0800

    Add image to the app and update the image resource with rolled dice result

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -16,15 +17,29 @@ class MainActivity : AppCompatActivity() {
         * - set button to change textview */
         val rollButton = findViewById<Button>(R.id.button)
         rollButton.setOnClickListener { rollDice() }
+
+        rollDice()
     }
 
     private fun rollDice() {
-        val resultTextView = findViewById<TextView>(R.id.textView)
+        val diceImage = findViewById<ImageView>(R.id.imageView)
         val dice = Dice(6)
         val diceRoll = dice.roll()
 
         Toast.makeText(this, "Dice Rolled", Toast.LENGTH_SHORT).show()
-        resultTextView.text = diceRoll.toString();
+
+        // Determine which drawable resource ID to use based on the dice roll
+        val drawableResource = when(diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource) // Update the ImageView with the correct drawable resource ID
+        diceImage.contentDescription = diceRoll.toString() // Update the content description
     }
 }
 
